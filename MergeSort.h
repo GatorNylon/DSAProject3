a56da1e6e6bb79e9444b7f5386b40607ca9aca06
alon2608 <alonoziel@gmail.com>
2021-04-14 13:46:07 -0400

    Update MergeSort

--- MergeSort.h
+++ MergeSort.h
@@ -3,7 +3,7 @@
 using namespace std;
 
 void merge(vector<Emissions> &arr, int left, int middle, int right, int type) {
-    vector<Emissions> sorted(arr.size(), Emissions("", "", "", "", 0, "", "", 0.0)); // Create a new vector that will hold the sorted values.
+    vector<Emissions> sorted; // Create a new vector that will hold the sorted values.
     
     int leftTemp = left;
     int rightTemp = middle + 1;
@@ -13,44 +13,44 @@ void merge(vector<Emissions> &arr, int left, int middle, int right, int type) {
         switch (type) {
             case 1:
                 if (arr[rightTemp].getState() > arr[leftTemp].getState()) { // Check if the state name is "bigger" in the right subarray than the left subarray. Add to "sorted" array if it is.
-                    sorted[sortedTemp] = arr[leftTemp];
+                    sorted.push_back(arr[leftTemp]);
                     ++sortedTemp;
                     ++leftTemp;
                 } else {
-                    sorted[sortedTemp] = arr[rightTemp];
+                    sorted.push_back(arr[rightTemp]);
                     ++sortedTemp;
                     ++rightTemp;
                 }
                 break;
             case 2:
                 if (arr[rightTemp].getZip() > arr[leftTemp].getZip()) { // Check if the zip code is "bigger" in the right subarray than the left subarray. Add to "sorted" array if it is.
-                    sorted[sortedTemp] = arr[leftTemp];
+                    sorted.push_back(arr[leftTemp]);
                     ++sortedTemp;
                     ++leftTemp;
                 } else {
-                    sorted[sortedTemp] = arr[rightTemp];
+                    sorted.push_back(arr[rightTemp]);
                     ++sortedTemp;
                     ++rightTemp;
                 }
                 break;
             case 3:
                 if (arr[rightTemp].getCity() > arr[leftTemp].getCity()) { // Check if the city name is "bigger" in the right subarray than the left subarray. Add to "sorted" array if it is.
-                    sorted[sortedTemp] = arr[leftTemp];
+                    sorted.push_back(arr[leftTemp]);
                     ++sortedTemp;
                     ++leftTemp;
                 } else {
-                    sorted[sortedTemp] = arr[rightTemp];
+                    sorted.push_back(arr[rightTemp]);
                     ++sortedTemp;
                     ++rightTemp;
                 }
                 break;
             case 4:
                 if (isgreater(arr[rightTemp].getTotalReleases(), arr[leftTemp].getTotalReleases())) { // Check if the emissions released are "bigger" in the right subarray than the left subarray. Add to "sorted" array if it is.
-                    sorted[sortedTemp] = arr[leftTemp];
+                    sorted.push_back(arr[leftTemp]);
                     ++sortedTemp;
                     ++leftTemp;
                 } else {
-                    sorted[sortedTemp] = arr[rightTemp];
+                    sorted.push_back(arr[rightTemp]);
                     ++sortedTemp;
                     ++rightTemp;
                 }
@@ -59,12 +59,12 @@ void merge(vector<Emissions> &arr, int left, int middle, int right, int type) {
     }
     
     for (int i = leftTemp; i <= middle; i++) { // Whatever elements are left over in the left subarray, copy them over.
-        sorted[sortedTemp] = arr[i];
+        sorted.push_back(arr[i]);
         sortedTemp++;
     }
     
     for (int j = rightTemp; j <= right; j++) { // Whatever elements are left over in the right subarray, copy them over.
-        sorted[sortedTemp] = arr[j];
+        sorted.push_back(arr[j]);
         sortedTemp++;
     }
     
